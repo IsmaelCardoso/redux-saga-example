b1f9c6f0cc94d97a39c901fb5ca5edc95608c900
Ismael <ismael@sinqias-MacBook-Pro.local>
2021-12-24 09:08:27 -0300

    chore (initial state):  - Created cart types.ts  - Using types in cart(reducer.ts)

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,5 +1,14 @@
-const cart = () => {
-    return []
+import { Reducer } from "react";
+import { Action } from "redux";
+import { ICartState } from "./types";
+
+const INITIAL_STATE: ICartState  = {
+    items: []
+}
+
+
+const cart: Reducer<ICartState, Action> = () => {
+    return INITIAL_STATE;
 }
 
 export default cart;
