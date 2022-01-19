b74f17a29f00f4e374182f4d23d59c944b1847b9
IsmaelCardoso <ismael.cardosos@gmail.com>
2022-01-19 00:08:43 -0300

    chore(saga(cart)):  Getting the current quantity in stock

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,6 +1,22 @@
-import { all, takeLatest } from 'redux-saga/effects'
+import { all, takeLatest, select } from 'redux-saga/effects'
+import { IState } from '../..';
+import { addProductToCart } from './action';
 
-const checkProductStock = () => {
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>
+
+function* checkProductStock ({ payload }: CheckProductStockRequest) {
+    const { product } = payload;
+
+    const currentQuantity: number = yield select((state: IState) => {
+        return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0 
+        /**
+         * ?.quantity é o valor a ser retornado caso a condição seja verdadeira
+         * "?? 0" = "|| 0" é a mesma coisa, será retornado caso a condição for falsa
+         */
+    })
+
+
+
     console.log("Add to cart!") 
 }
 
