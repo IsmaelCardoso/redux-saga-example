60d93cdc63a711b814094ed9b181a53fd7d48944
IsmaelCardoso <ismael.cardosos@gmail.com>
2022-01-18 21:43:11 -0300

    chore(updating actions):  - create cart component  - config updating action state

---
+++ src/components/Cart.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const Cart = () => {
+    const state = useSelector(state => state);
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Produto</th>
+                    <th>Preço</th>
+                    <th>Quantidade</th>
+                    <th>Subtotal</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    )
+}
+
+export default Cart;
--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,17 +1,41 @@
 import { Reducer } from "react";
 import { Action } from "redux";
-import { ICartState } from "./types";
+import { ICartState, IProduct } from "./types";
 
 const INITIAL_STATE: ICartState  = {
     items: []
 }
 
+interface payload {
+   product: IProduct 
+}
+interface IAction extends Action {
+    payload: payload;
+}
 
-const cart: Reducer<ICartState, Action> = (state, action) => {
-    console.log("state", state)
-    console.log("action", action)
 
-    return INITIAL_STATE;
+const cart: Reducer<ICartState, IAction> = (state = INITIAL_STATE, action) => {
+    switch(action.type) {
+        case "ADD_PRODUCT_TO_CART": {
+            const { product } = action.payload
+
+            return {
+                ...state,
+                itens: [
+                    ...state.items,
+                    {
+                        product,
+                        quantity: 1,
+
+                    }
+                ]
+
+            }
+        }
+        default: {
+            return state;
+        }
+    }
 }
 
 export default cart;
